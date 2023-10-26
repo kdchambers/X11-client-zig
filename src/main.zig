7061e38edb4fb4c00dd417f180210bde35c56c9b
Keith Chambers <keithchambers@protonmail.com>
2023-10-25 23:20:53 -0500

    implemented basic X11 client

---
+++ src/main.zig
@@ -0,0 +1,233 @@
+const std = @import("std");
+const x11 = @cImport({
+    @cInclude("X11/Xlib.h");
+    @cInclude("X11/Xutil.h");
+});
+
+var close_requested: bool = false;
+
+fn onDelete(display: *x11.Display, window: x11.Window) void {
+    _ = x11.XDestroyWindow(display, window);
+    close_requested = true;
+}
+
+var mouse_coordinates: struct { x: c_int, y: c_int } = .{ .x = 0, .y = 0 };
+
+inline fn keysymToChar(keysym: x11.KeySym, shift: bool) ?u8 {
+    const base_char: u8 = switch (keysym) {
+        x11.XK_space => ' ',
+        x11.XK_KP_0 => '0',
+        x11.XK_KP_1 => '1',
+        x11.XK_KP_2 => '2',
+        x11.XK_KP_3 => '3',
+        x11.XK_KP_4 => '4',
+        x11.XK_KP_5 => '5',
+        x11.XK_KP_6 => '6',
+        x11.XK_KP_7 => '7',
+        x11.XK_KP_8 => '8',
+        x11.XK_KP_9 => '9',
+
+        x11.XK_a => 'a',
+        x11.XK_b => 'b',
+        x11.XK_c => 'c',
+        x11.XK_d => 'd',
+        x11.XK_e => 'e',
+        x11.XK_f => 'f',
+        x11.XK_g => 'g',
+        x11.XK_h => 'h',
+        x11.XK_i => 'i',
+        x11.XK_j => 'j',
+        x11.XK_k => 'k',
+        x11.XK_l => 'l',
+        x11.XK_m => 'm',
+        x11.XK_n => 'n',
+        x11.XK_o => 'o',
+        x11.XK_p => 'p',
+        x11.XK_q => 'q',
+        x11.XK_r => 'r',
+        x11.XK_s => 's',
+        x11.XK_t => 't',
+        x11.XK_u => 'u',
+        x11.XK_v => 'v',
+        x11.XK_w => 'w',
+        x11.XK_x => 'x',
+        x11.XK_y => 'y',
+        x11.XK_z => 'z',
+        x11.XK_A => 'A',
+        x11.XK_B => 'B',
+        x11.XK_C => 'C',
+        x11.XK_D => 'D',
+        x11.XK_E => 'E',
+        x11.XK_F => 'F',
+        x11.XK_G => 'G',
+        x11.XK_H => 'G',
+        x11.XK_I => 'I',
+        x11.XK_J => 'J',
+        x11.XK_K => 'K',
+        x11.XK_L => 'L',
+        x11.XK_M => 'M',
+        x11.XK_N => 'N',
+        x11.XK_O => 'O',
+        x11.XK_P => 'P',
+        x11.XK_Q => 'Q',
+        x11.XK_R => 'R',
+        x11.XK_S => 'S',
+        x11.XK_T => 'T',
+        x11.XK_U => 'U',
+        x11.XK_V => 'V',
+        x11.XK_W => 'W',
+        x11.XK_X => 'X',
+        x11.XK_Y => 'Y',
+        x11.XK_Z => 'Z',
+
+        x11.XK_0 => '0',
+        x11.XK_1 => '1',
+        x11.XK_2 => '2',
+        x11.XK_3 => '3',
+        x11.XK_4 => '4',
+        x11.XK_5 => '5',
+        x11.XK_6 => '6',
+        x11.XK_7 => '7',
+        x11.XK_8 => '8',
+        x11.XK_9 => '9',
+        // x11.XK_ => '',
+
+        else => return null,
+    };
+
+    if (shift and (base_char >= 'a' and base_char <= 'z')) {
+        const caps_offset: u8 = 'a' - 'A';
+        return base_char - caps_offset;
+    }
+
+    return base_char;
+}
+
+var shift_mod_left: bool = false;
+var shift_mod_right: bool = false;
+
+pub fn main() !void {
+    var display: *x11.Display = x11.XOpenDisplay(null) orelse {
+        std.log.err("Failed to open X11 display", .{});
+        return error.OpenX11DisplayFail;
+    };
+    errdefer _ = x11.XCloseDisplay(display);
+
+    std.log.info("X11 display created", .{});
+
+    var root: x11.Window = x11.DefaultRootWindow(display);
+    if (root == x11.None) {
+        std.log.err("Failed to find root x11 window", .{});
+        return error.FindRootX11WindowFail;
+    }
+
+    var window: x11.Window = x11.XCreateSimpleWindow(display, root, 0, 0, 800, 600, 0, 0, 0xffffffff);
+    if (window == x11.None) {
+        std.log.err("Failed to create an x11 window", .{});
+        return error.CreateX11WindowFail;
+    }
+
+    _ = x11.XMapWindow(display, window);
+
+    var wm_delete_window: x11.Atom = x11.XInternAtom(display, "WM_DELETE_WINDOW", x11.False);
+    _ = x11.XSetWMProtocols(display, window, &wm_delete_window, 1);
+
+    _ = x11.XSetStandardProperties(display, window, "X11 Client in Zig", "X11 client", x11.None, null, 0, null);
+
+    // Available masks:
+    //   KeyPress,
+    //   KeyRelease,
+    //   ButtonPress,
+    //   ButtonRelease,
+    //   PointerMotion,
+    //   Button1Motion,
+    //   Button2Motion,
+    //   Button3Motion,
+    //   Button4Motion,
+    //   Button5Motion,
+    //   ButtonMotion
+    // src: https://tronche.com/gui/x/xlib/window/attributes/event-and-do-not-propagate.html
+    _ = x11.XSelectInput(display, window, x11.ExposureMask | x11.ButtonPressMask | x11.KeyPressMask | x11.KeyReleaseMask | x11.PointerMotionMask);
+
+    var event: x11.XEvent = undefined;
+    while (!close_requested) {
+        _ = x11.XNextEvent(display, &event);
+        switch (event.type) {
+            x11.ClientMessage => {
+                std.log.info("Client message recieved", .{});
+                if (event.xclient.data.l[0] == wm_delete_window) {
+                    onDelete(event.xclient.display.?, event.xclient.window);
+                }
+            },
+            x11.KeyPress => {
+                const keypress_event: *x11.XKeyEvent = @ptrCast(&event);
+                const keysym: x11.KeySym = x11.XLookupKeysym(keypress_event, 0);
+
+                if (keysym == x11.XK_Shift_L) {
+                    shift_mod_left = true;
+                    continue;
+                }
+
+                if (keysym == x11.XK_Shift_R) {
+                    shift_mod_right = true;
+                    continue;
+                }
+
+                if (keysymToChar(keysym, shift_mod_left or shift_mod_right)) |ch| {
+                    std.log.info("Pressed: {c}", .{ch});
+                }
+            },
+            x11.KeyRelease => {
+                const keypress_event: *x11.XKeyEvent = @ptrCast(&event);
+                const keysym: x11.KeySym = x11.XLookupKeysym(keypress_event, 0);
+
+                if (keysym == x11.XK_Shift_L) {
+                    shift_mod_left = false;
+                    continue;
+                }
+
+                if (keysym == x11.XK_Shift_R) {
+                    shift_mod_right = false;
+                    continue;
+                }
+            },
+            x11.ButtonPress => std.log.info("XEvent: ButtonPress", .{}),
+            x11.ButtonRelease => std.log.info("XEvent: ButtonRelease", .{}),
+            x11.MotionNotify => {
+                const motion_event: *x11.XMotionEvent = @ptrCast(&event);
+                mouse_coordinates.x = motion_event.x;
+                mouse_coordinates.y = motion_event.y;
+                // std.debug.print("Mouse: ({d}, {d})\n", .{ mouse_coordinates.x, mouse_coordinates.y });
+            },
+            x11.EnterNotify => std.log.info("XEvent: EnterNotify", .{}),
+            x11.LeaveNotify => std.log.info("XEvent: LeaveNotify", .{}),
+            x11.FocusIn => std.log.info("XEvent: FocusIn", .{}),
+            x11.FocusOut => std.log.info("XEvent: FocusOut", .{}),
+            x11.KeymapNotify => std.log.info("XEvent: KeymapNotify", .{}),
+            x11.Expose => std.log.info("XEvent: Expose", .{}),
+            x11.GraphicsExpose => std.log.info("XEvent: GraphicsExpose", .{}),
+            x11.NoExpose => std.log.info("XEvent: NoExpose", .{}),
+            x11.VisibilityNotify => std.log.info("XEvent: VisibilityNotify", .{}),
+            x11.CreateNotify => std.log.info("XEvent: CreateNotify", .{}),
+            x11.DestroyNotify => std.log.info("XEvent: DestroyNotify", .{}),
+            x11.UnmapNotify => std.log.info("XEvent: UnmapNotify", .{}),
+            x11.MapNotify => std.log.info("XEvent: MapNotify", .{}),
+            x11.MapRequest => std.log.info("XEvent: MapRequest", .{}),
+            x11.ReparentNotify => std.log.info("XEvent: ReparentNotify", .{}),
+            x11.ConfigureNotify => std.log.info("XEvent: ConfigureNotify", .{}),
+            x11.ConfigureRequest => std.log.info("XEvent: ConfigureRequest", .{}),
+            x11.GravityNotify => std.log.info("XEvent: GravityNotify", .{}),
+            x11.ResizeRequest => std.log.info("XEvent: ResizeRequest", .{}),
+            x11.CirculateNotify => std.log.info("XEvent: CirculateNotify", .{}),
+            x11.CirculateRequest => std.log.info("XEvent: CirculateRequest", .{}),
+            x11.PropertyNotify => std.log.info("XEvent: PropertyNotify", .{}),
+            x11.SelectionClear => std.log.info("XEvent: SelectionClear", .{}),
+            x11.SelectionRequest => std.log.info("XEvent: SelectionRequest", .{}),
+            x11.SelectionNotify => std.log.info("XEvent: SelectionNotify", .{}),
+            x11.ColormapNotify => std.log.info("XEvent: ColormapNotify", .{}),
+            x11.MappingNotify => std.log.info("XEvent: MappingNotify", .{}),
+            x11.GenericEvent => std.log.info("XEvent: GenericEvent", .{}),
+            else => std.log.info("Unknown event with type: {d}", .{event.type}),
+        }
+    }
+}
